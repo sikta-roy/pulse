fd840264e6a847518891b2c44daf0e89bc199dae
Sikta Roy <siktaroy27@gmail.com>
2024-07-14 22:49:22 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://tops-glider-82.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://tops-glider-82.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lovely-flamingo-139.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'marvelous-crane-118.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com'
+        },
+      ]
+    }
+  };
+  
+  export default nextConfig;
